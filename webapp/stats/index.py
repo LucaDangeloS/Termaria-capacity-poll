485c3fd9a2b6a7927cb9dd7be5669297fccbf9ac
LucaDangeloS <lucad7@gmail.com>
2024-05-22 11:55:52 +0200

    Removed day periods chart

--- webapp/stats/index.py
+++ webapp/stats/index.py
@@ -7,23 +7,11 @@ from webapp.stats.charts.temporal_comparison import *
 from webapp.stats.charts.season_chart import *
 from webapp.stats.charts.heatmap import *
 from webapp.stats.charts.trends import *
-from webapp.stats.charts.day_periods_chart import *
 
 '''
 IDEAS:
-- Media de personas por día anualmente *
-- Comparación temporal en períodos:
-    - Gráfica comparativa de aforo medio mensual por años
-    - Gráfica comparativa de aforo medio diario por años
-    - Estacionalidad en aforo por años
-    - Comparación de la media de gente este año en relación al año pasado.
-    - Comparación de la media de gente el año pasado en relación al anterior.
-    - Comparación de la media de gente en un mes en relación al mes anterior.
-- Estacionalidad en aforo
 - Heatmap de los días más concurridos en relación la media
 - Tendencia del aforo a lo largo de los meses de los últimos dos años
-- Comparación de la diferencia de gente entre distintos períodos (mañana, principios de tarde, finales de tarde/noche)
-Make a temporal comparison (mopnth after month, year after year) with bars marking the difference
 '''
 
 def historic_stats_data(get_data):
@@ -38,7 +26,6 @@ def historic_stats_data(get_data):
             'Por estaciones': season_chart,
             'Mapa de calor por días': heatmap,
             'Tendencias': trends,
-            'Por períodos del día': day_periods_chart,
         }
 
         selected_chart = option_menu(
